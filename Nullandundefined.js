a1f856ff475db22c6f1dc0d2058768972c2cc1c0
Isaac025 <isaacolayiwola85@gmail.com>
2025-03-11 07:48:36 -0700

    boolean and comparison added

---
+++ Nullandundefined.js
@@ -0,0 +1,14 @@
+// NULL AND UNDEFINED
+
+let user = null; // falsy value
+// a b c d x y z
+
+// x (no product found)
+
+//after log in
+
+user = "Babathiago";
+
+let myname; //falsy value
+
+console.log(myname);
